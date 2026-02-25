1226e57dc254c7074d7161f3d84ff990e26b3595
marco-samembe <imaramwamba5@gmail.com>
2026-02-25 10:44:57 +0300

    backend upload improved

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const storage = multer.diskStorage({
- destination: function(req,file,cb){
-  cb(null,"uploads/");
- },
- filename: function(req,file,cb){
-  cb(null,file.originalname);
- }
+app.use("/files",
+express.static("uploads"));
+
+const storage =
+multer.diskStorage({
+
+destination:function(req,file,cb){
+cb(null,"uploads/");
+},
+
+filename:function(req,file,cb){
+cb(null,file.originalname);
+}
+
 });
 
-const upload = multer({storage:storage});
+const upload =
+multer({storage:storage});
 
-app.post("/upload",upload.single("file"),(req,res)=>{
- res.send("File uploaded");
+app.post("/upload",
+upload.single("file"),
+(req,res)=>{
+
+res.json({
+message:"Uploaded",
+file:req.file.filename
+});
+
 });
 
 app.listen(3000,()=>{
- console.log("Server running on port 3000");
+
+console.log("Running 3000");
+
 });
